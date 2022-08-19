47e19cce276ce23c811afa12107451f9504c5436
ayrtonaoki <ayrtonsasouza@gmail.com>
2022-08-19 00:03:36 -0300

    Create values_sum and brt_time_zone tip methods

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,2 +1,11 @@
 class Tip < ApplicationRecord
+    def self.values_sum
+        Tip.all.pluck(:value).sum
+    end
+
+    def brt_time_zone
+        Time.use_zone("America/Sao_Paulo") do
+            Time.zone.parse(self.created_at.to_s).strftime("%d/%m/%Y - %H:%M:%S") 
+        end
+    end
 end
